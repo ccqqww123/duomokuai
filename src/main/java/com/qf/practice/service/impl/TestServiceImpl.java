a97fa51a2a8f0b91ddfb95b56f8d9f0d76eac7fc
chenqiwen <3204194018@qq.com>
2022-09-02 09:54:26 +0800

    提交了一个没有bug的业务层

---
+++ src/main/java/com/qf/practice/service/impl/TestServiceImpl.java
@@ -0,0 +1,15 @@
+package com.qf.practice.service.impl;
+
+import com.qf.practice.service.TestService;
+
+public class TestServiceImpl implements TestService {
+    @Override
+    public int save() {
+        return 0;
+    }
+
+    @Override
+    public int update() {
+        return 0;
+    }
+}
